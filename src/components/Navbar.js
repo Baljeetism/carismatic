223f8088fc7ab15ffccd43eb6cef2f3c70536779
Baljeet Singh <57327580+Baljeetism@users.noreply.github.com>
2023-06-02 02:17:33 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div className='navbar-con'>
       {/* <h2 className='h2footer'>Search</h2> */}
       <Link style={{textDecoration: 'none'}} to="/search"><h2 className='h2footer'>Search</h2></Link>
-      <Link className='hau' style={{textDecoration: 'none'}} to="/"><h2 className='h2footer'>CARismatic</h2></Link>
+      <Link className='hau' style={{textDecoration: 'none'}} to="/carismatic"><h2 className='h2footer'>CARismatic</h2></Link>
       {/* <span style={{textDecoration: 'none'}} className='hau'>
       <Link to="/"><h2 className='h2footer'>CARismatic</h2></Link>
       </span> */}
